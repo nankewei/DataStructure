a948010e640093fbfa99213a53ef05f95a25c9fb
nankewei <35093286+nankewei@users.noreply.github.com>
2022-11-08 10:59:09 +0800

    Create Stack_ArrayImplementation.c

---
+++ Stack_ArrayImplementation.c
@@ -0,0 +1,55 @@
+// Stack - Array based implementation.
+// Creating a stack of integers.
+#include <stdio.h>
+
+#define MAX_SIZE 101
+int A[MAX_SIZE];
+int top = -1;
+
+void Push(int x)
+{
+    if (top == MAX_SIZE - 1)
+    {
+        printf("Error: stack overflow\n");
+        return;
+    }
+    printf("Push number is %d\n", x);
+    A[++top] = x;
+}
+void Pop()
+{
+    if (top == -1)
+    {
+        printf("Error: No element to pop\n");
+        return;
+    }
+    printf("Now pop the number %d\n", A[top]);
+    top--;
+}
+int Top()
+{
+    return A[top];
+}
+void Print()
+{
+    printf("stack : ");
+    for (int i = 0; i <= top; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    putchar('\n');
+}
+int main()
+{
+    Push(2);
+    Print();
+    Push(5);
+    Print();
+    Push(10);
+    Print();
+    Pop();
+    Print();
+    Push(12);
+    Print();
+    return 0;
+}
